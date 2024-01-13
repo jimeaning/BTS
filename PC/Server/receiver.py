460fad3c96e2b30822ee3af5ecc7b55f7e1f1b3b
jimeaning <kjimin63@naver.com>
2024-01-13 20:32:43 +0900

    feat: 라즈베리파이와 GUI UDP 통신 기능 추가

---
+++ PC/Server/receiver.py
@@ -0,0 +1,44 @@
+import socket
+from PyQt5.QtCore import QThread, pyqtSignal
+
+server_ip = "10.10.15.106"
+server_port = 4000
+        
+
+class ReceiveMessage(QThread):
+    rcv_msg_signal = pyqtSignal()
+    
+    def __init__(self, msg_queue):
+        super().__init__()        
+        self.msg_queue = msg_queue
+        
+        self.host = server_ip
+        self.port = server_port
+        self.udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_server_socket.bind((self.host, self.port))
+        self.udp_server_socket.setblocking(False)
+        print(f"서버 대기 중... {self.host}:{self.port}")
+        
+    def __del__(self):
+        self.server_socket.close()
+        print("서버를 닫습니다..")
+
+    def run(self):
+        buffersize = 1024    
+        
+        print("UDP server is up and listening")
+
+        while(True):
+            try:
+                byte_addr_pair = self.udp_server_socket.recvfrom(buffersize)
+            except BlockingIOError:
+                continue
+            
+            msg = byte_addr_pair[0]
+            msg = msg.decode("utf-8")
+            print(msg)   
+            
+            if self.msg_queue:
+                self.msg_queue.put(msg)
+            
+            self.rcv_msg_signal.emit()
